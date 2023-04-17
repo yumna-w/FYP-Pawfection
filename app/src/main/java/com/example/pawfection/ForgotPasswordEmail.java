0b1eda05adc7a8ace0a025d4311af9d0047d4465
Labeenah <127021267+Labeenah@users.noreply.github.com>
2023-04-17 23:58:23 +0500

    added Forgot Password Screens

---
+++ app/src/main/java/com/example/pawfection/ForgotPasswordEmail.java
@@ -0,0 +1,14 @@
+package com.example.pawfection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPasswordEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password_email);
+    }
+}
