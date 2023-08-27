1fcd4c2554cf2822ab5b84992657c63c30520778
Labeenah Khan <labeenah1310@gmail.com>
2023-08-28 02:52:32 +0500

    vet appointments view & vet appointments list

---
+++ app/src/main/java/com/example/pawfection/VetAppointmentListItem.java
@@ -0,0 +1,14 @@
+package com.example.pawfection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VetAppointmentListItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vet_appointment_list_item);
+    }
+}
---
+++ app/src/main/java/com/example/pawfection/VetAppointmentsViewAppointments.java
@@ -0,0 +1,14 @@
+package com.example.pawfection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VetAppointmentsViewAppointments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vet_appointments_view_appointments);
+    }
+}
